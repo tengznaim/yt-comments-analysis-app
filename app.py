0077ca5aac77a00abc640d19ca630a45f94eab21
Tengku Naim <naimtmas@gmail.com>
2022-01-17 23:27:12 +0800

    updated regex and added wordcloud function

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import re
 
-from utils import get_video_comments
+from utils import get_video_comments, generate_worldcloud
 
 st.set_page_config(page_title="YouTube Video Comments Sentiment Analysis App")
 st.title("YouTube Video Comments Sentiment Analysis App")
@@ -13,13 +13,21 @@ video_url = st.text_input(
 submit_button = st.button("Submit")
 
 if submit_button:
+    print(video_url)
     valid_url = re.match(
-        "^(http(s)?:\/\/(www)?)?youtube.com\/watch\?v=.+", video_url)
+        "^(http(s)?:\/\/(www.)?)?youtube.com\/watch\?v=.+", video_url)
+
     if valid_url:
         video_id = video_url[video_url.index("=") + 1:]
         comments_list = get_video_comments(video_id)
 
         df = pd.DataFrame(comments_list)
-        st.dataframe(df)
+        filtered_df = df[["textDisplay", "authorDisplayName",
+                          "likeCount", "publishedAt", "updatedAt"]]
+        st.dataframe(filtered_df)
+
+        video_wordcloud = generate_worldcloud(comments_list)
+        st.image(video_wordcloud)
+
     else:
         st.error("Please enter a valid video URL")
--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 import requests
 import json
+from wordcloud import WordCloud
 
 load_dotenv()
 
@@ -39,3 +40,22 @@ def get_video_comments(video_id: str):
         comments_list.append(comment_info)
 
     return comments_list
+
+
+def generate_worldcloud(comments_list: list):
+    """Generates a wordcloud from the API response.
+
+    Args:
+        comments_list (list): List of items properties from the commentThreads response.
+
+    Returns:
+        video_world (np.array): Wordcloud in the form of a numpy array for use in the Streamlit app.
+    """
+
+    comments_text = [comment["textDisplay"] for comment in comments_list]
+    all_comments = " ".join(comments_text)
+
+    video_wordcloud = WordCloud(
+        background_color="white", width=1280, height=720).generate(all_comments)
+
+    return video_wordcloud.to_array()
